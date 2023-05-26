b27afe10f27b51a193afc19dc993963b52f4dd2d
kevinkeven20 <kevinkeven20@gmail.com>
2023-05-27 01:41:25 +0300

    changed base dir

--- gilead/settings.py
+++ gilead/settings.py
@@ -17,7 +17,7 @@ import os
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
