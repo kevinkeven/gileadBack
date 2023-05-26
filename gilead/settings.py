02f04a9dd9f2666c2324733bc0cdfbdd22142d33
kevinkeven20 <kevinkeven20@gmail.com>
2023-05-27 01:43:52 +0300

    debug set to false

--- gilead/settings.py
+++ gilead/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ["*"]
 
 ADMINS = [("Kevinkeven", "kevinkevendev@gmail.com")]
