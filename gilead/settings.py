a25a1360eed5e7c8e6fde0a7b6f19e953867066d
kevinkeven20 <kevinkeven20@gmail.com>
2023-05-27 01:27:51 +0300

    added statifiles dirs

--- gilead/settings.py
+++ gilead/settings.py
@@ -185,6 +185,7 @@ STATIC_URL = "/static/"
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_ROOT = BASE_DIR / "media"
 STATIC_ROOT = BASE_DIR / "staticfiles/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
