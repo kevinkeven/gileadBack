e3f4f99f66d066346c86727d42e523331a9a5108
kevinkeven20 <kevinkeven20@gmail.com>
2023-05-28 11:15:59 +0300

    removed django_on_heroku

--- gilead/settings/base.py
+++ gilead/settings/base.py
@@ -165,6 +165,3 @@ STATIC_ROOT = Path(BASE_DIR, "staticfiles/")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-import django_on_heroku
-
-django_on_heroku.settings(locals())
