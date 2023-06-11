21d16395b3dcf25cfa639457b5cc2044d3e94a24
kevinkeven20 <kevinkeven20@gmail.com>
2023-06-11 21:07:22 +0300

    commited

--- gilead/settings.py
+++ gilead/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+from decouple import Config
 
+config = Config()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
