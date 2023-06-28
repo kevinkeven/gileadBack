d7c794e98418c34ceb4467c90b12d3dde435d6e5
kevinkeven20 <kevinkeven20@gmail.com>
2023-06-28 20:08:49 +0300

    updated for cpanel

--- gilead/settings.py
+++ gilead/settings.py
@@ -72,23 +72,23 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 ]
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "gilerkjy_gilead",
-        "USER": "gilerkjy_gilead",
-        "PASSWORD": "admin@gilead",
-        "HOST": "localhost",
-        "PORT": 3306,
-        "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
-    }
-}
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "gilerkjy_gilead",
+#         "USER": "gilerkjy_gilead",
+#         "PASSWORD": "admin@gilead",
+#         "HOST": "localhost",
+#         "PORT": 3306,
+#         "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
 #     }
 # }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 ROOT_URLCONF = "gilead.urls"
 
@@ -153,9 +153,7 @@ EMAIL_HOST = "smtp.sendgrid.net"
 EMAIL_PORT = 25
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "apikey"
-EMAIL_HOST_PASSWORD = (
-    "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"
-)
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", cast=str, default="api")
 
 
 # Internationalization
