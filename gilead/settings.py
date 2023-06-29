a42170b837058e36879ea72338c8cad0ae178341
kevinkeven20 <kevinkeven20@gmail.com>
2023-06-29 16:55:48 +0300

    updated heroku settings

--- gilead/settings.py
+++ gilead/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import django_on_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -198,3 +199,5 @@ CSRF_COOKIE_SECURE = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+django_on_heroku.settings(locals())
