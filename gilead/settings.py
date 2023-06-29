d32bafc248cfa8b0d3da9a1f786718cbf6f453af
kevinkeven20 <kevinkeven20@gmail.com>
2023-06-29 12:51:25 +0300

    changed

--- gilead/settings.py
+++ gilead/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY", cast=str, default="key")
+SECRET_KEY = config("SECRET_KEY", cast=str, default="keey")
 
 # ADMINS = config("ADMINS", cast=lambda v: [s.strip() for s in v.split(",")])
 
