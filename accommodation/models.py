f07f395be22d11929ae7fc4c4f58252319ec8366
kevinkeven20 <kevinkeven20@gmail.com>
2023-07-03 16:37:13 +0300

    changed accommodation insider tip max_length

--- accommodation/models.py
+++ accommodation/models.py
@@ -64,7 +64,7 @@ class ExpertView(models.Model):
     accommodation = models.ForeignKey(
         Accommodation, on_delete=models.CASCADE, related_name="expertview"
     )
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=500)
 
     def __str__(self):
         return self.name
