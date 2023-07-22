7fe9ef7d61c09489fdc84515d8e33273340a83ed
kevinkeven20 <kevinkeven20@gmail.com>
2023-07-23 01:06:22 +0300

    changed settings

--- gilead/settings.py
+++ gilead/settings.py
@@ -64,7 +64,6 @@ INSTALLED_APPS = [
     "destination",
     "itineraries",
     "search",
-    "mdeditor",
     "shared",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -149,73 +148,6 @@ EMAIL_PORT = 25
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "apikey"
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", cast=str, default="api")
-
-X_FRAME_OPTIONS = "SAMEORIGIN"
-
-MDEDITOR_CONFIGS = {
-    "default": {
-        "width": "100% ",  # Custom edit box width
-        "height": 700,  # Custom edit box height
-        "toolbar": [
-            "undo",
-            "redo",
-            "|",
-            "bold",
-            "del",
-            "italic",
-            "quote",
-            "ucwords",
-            "uppercase",
-            "lowercase",
-            "|",
-            "h1",
-            "h2",
-            "h3",
-            "h5",
-            "h6",
-            "|",
-            "list-ul",
-            "list-ol",
-            "hr",
-            "|",
-            "link",
-            "reference-link",
-            "image",
-            "code",
-            "preformatted-text",
-            "code-block",
-            "table",
-            "datetime",
-            "emoji",
-            "html-entities",
-            "pagebreak",
-            "goto-line",
-            "|",
-            "help",
-            "info",
-            "||",
-            "preview",
-            "watch",
-            "fullscreen",
-        ],  # custom edit box toolbar
-        # image upload format type
-        "upload_image_formats": ["jpg", "jpeg", "gif", "png", "bmp", "webp", "svg"],
-        "image_folder": "editor",  # image save the folder name
-        "theme": "default",  # edit box theme, dark / default
-        "preview_theme": "default",  # Preview area theme, dark / default
-        "editor_theme": "default",  # edit area theme, pastel-on-dark / default
-        "toolbar_autofixed": False,  # Whether the toolbar capitals
-        "search_replace": True,  # Whether to open the search for replacement
-        "emoji": True,  # whether to open the expression function
-        "tex": True,  # whether to open the tex chart function
-        "flow_chart": True,  # whether to open the flow chart function
-        "sequence": True,  # Whether to open the sequence diagram function
-        "watch": True,  # Live preview
-        "lineWrapping": True,  # lineWrapping
-        "lineNumbers": True,  # lineNumbers
-        "language": "en",  # zh / en / es
-    }
-}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
